ad01e9a82413e6024207a3be3314a805b7c7e9dd
Stefano Mariani <stefano.mariani@unimore.it>
2023-12-04 08:17:08 +0100

    sb3 a2c mlp ok

--- slime_environments/agents/sb3/a2c-mlp.py
+++ slime_environments/agents/sb3/a2c-mlp.py
@@ -9,10 +9,10 @@ with open(PARAMS_FILE) as f:
 env = gym.make("Slime-v0", **params)
 
 model = A2C('MlpPolicy', env, verbose=2)  # 2=debug
-model.learn(total_timesteps=100*params['episode_ticks'])  # total env steps
+model.learn(total_timesteps=10*params['episode_ticks'])  # total env steps
 
 obs, _ = env.reset()
-for i in range(100):
+for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)  # QUESTION "deterministic actions"? what does it mean?
     obs, reward, _, _, _ = env.step(action)
     env.render()
