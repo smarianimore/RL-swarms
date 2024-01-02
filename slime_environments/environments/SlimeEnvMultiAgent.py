5d6895564d097a33d1096124159780d4f538e544
Francesca Palazzo <francip2000@gmail.com>
2024-01-02 17:59:15 +0100

    Created Ants Env

--- slime_environments/environments/SlimeEnvMultiAgent.py
+++ slime_environments/environments/SlimeEnvMultiAgent.py
@@ -664,30 +664,32 @@ class Slime(AECEnv):
 
 
 if __name__ == "__main__":
-    PARAMS_FILE = "multi-agent-env-params.json"
+    PARAMS_FILE = r"slime_environments\environments\multi-agent-env-params.json"
     EPISODES = 5
     LOG_EVERY = 1
 
     with open(PARAMS_FILE) as f:
         params = json.load(f)
+        
     if params["gui"]:
         render = "human"
     else:
         render = "server"
+        
     env = Slime(render_mode=render, **params)
 
     for ep in range(1, EPISODES + 1):
         env.reset()
-        print(
-            f"-------------------------------------------\nEPISODE: {ep}\n-------------------------------------------")
+        #print(f"-------------------------------------------\nEPISODE: {ep}\n-------------------------------------------")
         for tick in range(params['episode_ticks']):
-            for agent in env.agent_iter(max_iter=params["learner_population"]):
+            """for agent in env.agent_iter(max_iter=params["learner_population"]):
                 observation, reward, _ , _, info = env.last(agent)
                 env.step(env.action_space(agent).sample())
-            # env.evaporate_chemical()
-            # env.move()
-            # env._evaporate()
-            # env._diffuse()
-            # env.render()
+            """
+            #env.evaporate_chemical()
+            env.move()
+            env._evaporate()
+            env._diffuse()
+            env.render()
     env.close()
     
--- slime_environments/environments/pettingzoo-apitest.py
+++ slime_environments/environments/pettingzoo-apitest.py
@@ -1,9 +1,9 @@
-from slime_environments.environments.SlimeEnvMultiAgent import Slime
+from SlimeEnvMultiAgent import Slime
 import pettingzoo
 from pettingzoo.test import api_test
 import json
 
-PARAMS_FILE = "multi-agent-env-params.json"
+PARAMS_FILE = r"slime_environments\environments\multi-agent-env-params.json"
 
 with open(PARAMS_FILE) as f:
     params = json.load(f)
@@ -11,6 +11,7 @@ if params["gui"]:
     render = "human"
 else:
     render = "server"
+
 env = Slime(render_mode=render, **params)
 api_test(env, num_cycles=100, verbose_progress=True)
 print(f"Environment compatible with Gymnasium {pettingzoo.__version__=}")
