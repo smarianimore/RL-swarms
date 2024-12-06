4136c5dc2e2f23c116e61af4bdca92125475479e
dav3-b <davide24.borghi@gmail.com>
2024-12-06 14:16:42 +0100

    Deleted commented code

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-#setup(
-    #name="slime_environments",
-    #version="1.0.0",
-    #packages=find_packages(),
-    #install_requires=['gymnasium', 'pygame']
-#)
 setup(
     name="rl_swarms",  # Nome del pacchetto principale
     version="0.0.1",  # Versione
