344fb75f4d3fed92a9cd931cdd149e4a52d7a074
dav3-b <davide24.borghi@gmail.com>
2024-12-06 10:26:00 +0100

    Deleted a bunch of files no longer useful

--- refactoring/main.py
+++
@@ -1,9 +0,0 @@
-from tests.slime import pettingzoo_api, slime
-
-PARAMS_FILE = "jaxenvs/configs/slime/multi_agent_env_params.json"
-EPISODES = 5
-LOG_EVERY = 1
-
-#pettingzoo_api.test_api(PARAMS_FILE)
-
-slime.test(PARAMS_FILE, EPISODES, LOG_EVERY)
